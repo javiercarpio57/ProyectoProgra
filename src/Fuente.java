47118ed4b79d800c3afab1fccff44d080fcfdc06
josepablito16 <josepablocif16@gmail.com>
2017-08-14 22:37:19 -0600

    me decis si esta bien

---
+++ src/Fuente.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.util.ArrayList;
+
+/**
+ *
+ * @author jose
+ */
+public class Fuente {
+    
+    
+    
+    public Fuente()
+    {
+        
+        
+    }
+    
+    public void registro(String clase)
+    {
+        Ver_Registro c = new Ver_Registro();
+        c.j=clase;
+    
+    }
+    
+    
+    
+}
